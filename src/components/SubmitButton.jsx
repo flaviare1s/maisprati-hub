7a4700e6c41356db3df82d611cd5c67152e5b422
flaviare1s <flaviarster@gmail.com>
2025-08-13 11:35:23 -0300

    feat: login and input field component

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,5 +1,10 @@
-export const SubmitButton = () => {
+export const SubmitButton = ({ label }) => {
   return (
-    <div>SubmitButton</div>
-  )
-}
+    <button
+      type="submit"
+      className="text-light font-medium text-sm py-2 px-4 rounded-md transition-colors duration-75 font-inter focus:outline-none focus:shadow-outline w-full cursor-pointer mt-5 bg-red-logo hover:bg-red-secondary"
+    >
+      {label}
+    </button>
+  );
+};
