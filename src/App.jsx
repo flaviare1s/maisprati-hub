5fcdfb96074eb39031f3060a19b5abf6f70aa0c5
flaviare1s <flaviarster@gmail.com>
2025-08-13 17:14:06 -0300

    feat: add select account page

--- src/App.jsx
+++ src/App.jsx
@@ -45,10 +45,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<SelectAccountType />} />
-          <Route path="/register/user" element={<UserRegister />} />
-          <Route path="/register/clinic" element={<ClinicRegister />} />
+          <Route path="/register/donator" element={<UserRegister />} />
+          <Route path="/register/institution" element={<ClinicRegister />} />
           <Route path="/dashboard/user" element={<UserDashboard />} />
-          <Route path="/dashboard/clinic" element={<ClinicDashboard />} />
+          <Route path="/dashboard/institution" element={<ClinicDashboard />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/donations" element={<DonationHistory />} />
           <Route path="/appointments" element={<Appointments />} />
