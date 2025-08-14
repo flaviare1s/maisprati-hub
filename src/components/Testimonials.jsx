034d2733264928da9bc768d4adda2a9bc79e5e8a
flaviare1s <flaviarster@gmail.com>
2025-08-14 13:19:43 -0300

    feat: testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,66 @@
+import { Testimonial } from "./Testimonial";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import person1 from "../assets/images/person1.png";
+import person2 from "../assets/images/person2.png";
+import person3 from "../assets/images/person3.png";
+import person4 from "../assets/images/person4.png";
+
 export const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section className="py-16 md:py-2">
+      <div className="container mx-auto px-4 md:px-10">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={24}
+          slidesPerView={1}
+          loop={false}
+          navigation={true}
+          breakpoints={{
+            900: {
+              slidesPerView: 2,
+              spaceBetween: 24,
+            },
+            1400: {
+              slidesPerView: 3,
+              spaceBetween: 24,
+            }
+          }}
+
+        >
+          <SwiperSlide>
+            <Testimonial
+              img={person1}
+              name="Maria Conceição, 60 anos."
+              description="“Graças a uma doação anônima, pude passar por minha cirurgia com segurança. Nunca saberei quem foi, mas serei eternamente grata.”"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Testimonial
+              img={person2}
+              name="Pedro Cornelho, 5 anos."
+              description="“Meu filho recebeu transfusões durante meu tratamento contra o câncer. Cada bolsa de sangue foi um novo fôlego de vida. Obrigado a todos que doam! ”"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Testimonial
+              img={person3}
+              name="Debora Pereira, 35 anos."
+              description="“Tive um acidente de moto e precisei de sangue com urgência. Pessoas que eu nunca conheci me ajudaram. Hoje, sou doador também.”"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Testimonial
+              img={person4}
+              name="Luciano Costa, 42 anos."
+              description="“O sangue doado não salvou só a minha vida, mas também devolveu a esperança da minha familia”"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+};
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -4,15 +4,17 @@ import { Testimonials } from "../components/Testimonials"
 
 export const FAQ = () => {
   return (
-    <div className="flex flex-col items-center justify-center mx-auto p-5">
-      <SectionTitle title="Dúvidas" />
-      <div className="mb-10">
-        <FaqItem question="Qual a idade mínima para doar sangue?" answer="18 anos" />
-        <FaqItem question="Qualo peso mínimo para doar sangue?" answer="50kg" />
-        <FaqItem question="Tem alguma contraindicação para doar sangue?" answer="Sim. Algumas doenças podem impedir a doação de sangue" />
+    <div className="flex flex-col mx-auto p-5 w-full">
+      <div className="flex flex-col justify-center items-center">
+        <SectionTitle title="Dúvidas" />
+        <div className="mb-10">
+          <FaqItem question="Qual a idade mínima para doar sangue?" answer="18 anos" />
+          <FaqItem question="Qualo peso mínimo para doar sangue?" answer="50kg" />
+          <FaqItem question="Tem alguma contraindicação para doar sangue?" answer="Sim. Algumas doenças podem impedir a doação de sangue" />
+        </div>
+        <SectionTitle title="Agradecimentos" />
       </div>
-      <SectionTitle title="Agradecimentos" />
-      <div>
+      <div className="">
         <Testimonials />
       </div>
     </div>
