3f6c5dd1aeadbcc949b92888f965df08d7e53878
Wellington Junior <roxjunio1@gmail.com>
2025-08-14 20:53:39 -0300

    feat:faq

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,8 +1,28 @@
-export const FaqItem = ({ question, answer }) => {
+import { useState } from 'react';
+
+export const FaqItem = ({ question, answer, icon}) => {
+  const [isOpen, setIsOpen] = useState(false);
+    const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <h3>{ question }</h3>
-      <p>{ answer }</p>
+    <div className="border-b border-gray-200 mb-4"> {/* Este é o elemento pai */}
+      <button
+        className="flex justify-between items-center w-full p-4 text-left font-roboto font-bold text-[#444444] bg-[#E6938B] rounded-lg shadow-md"
+        onClick={toggleAccordion}
+      >
+        <span className="text-sm md:text-base">{question}</span>
+        <span>{icon}</span>
+        <span className={`transform transition-transform duration-300 ${isOpen ? 'rotate-180' : ''}`}>
+          &#9660;
+        </span>
+      </button>
+      {isOpen && (
+        <div className="py-2 text-gray-600">
+          <p>{answer}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
