9b26631c3f1cb5a681252bfa3322f9fe3765e0bb
flaviare1s <flaviarster@gmail.com>
2025-08-14 13:56:47 -0300

    style: adjust color

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,5 @@
 export const SectionTitle = ({ title }) => {
   return (
-    <h2 className="uppercase font-lg text-white bg-red-logo hover:bg-red-secondary py-2 px-6 rounded-full w-full sm:max-w-[500px] m-5 text-center font-bold">{ title }</h2>
+    <h2 className="uppercase font-lg text-white bg-red-secondary py-2 px-6 rounded-full w-full sm:max-w-[500px] m-5 text-center font-bold">{ title }</h2>
   )
 }
