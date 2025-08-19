920460fbd82a76d2814ac8f9eef79ef504cf600c
flaviare1s <flaviarster@gmail.com>
2025-08-19 15:10:10 -0300

    style: adjust style

--- src/App.jsx
+++ src/App.jsx
@@ -51,28 +51,7 @@ function App() {
       </main>
       <Footer />
       <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-          success: {
-            duration: 3000,
-            style: {
-              background: '#007DE3',
-              color: '#fff',
-            },
-          },
-          error: {
-            duration: 4000,
-            style: {
-              background: '#dc3545',
-              color: '#fff',
-            },
-          },
-        }}
+        position="top-center"
       />
     </div>
   );
