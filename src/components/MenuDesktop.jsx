48eead72422e9d14f25ccb7eb0e2e80616d74b37
flaviare1s <flaviarster@gmail.com>
2025-08-21 15:17:21 -0300

    fix: adjust desktop menu

--- src/components/MenuDesktop.jsx
+++ src/components/MenuDesktop.jsx
@@ -9,7 +9,7 @@ export const MenuDesktop = ({ user, onLogout }) => {
       {!user && <Link to='/register' className="uppercase font-lg text-light font-bold bg-orange-logo hover:text-blue-logo py-2 px-6 rounded-lg" >Cadastrar</Link>}
       {user && <Link to='/dashboard' className='uppercase font-lg text-blue-logo font-bold hover:text-orange-logo link-nav' >Dashboard</Link>}
       {user && <Link to='/common-room' className='uppercase font-lg text-blue-logo font-bold hover:text-orange-logo link-nav' >Taverna dos Heróis</Link>}
-      {user ? <Link to='/login' className="uppercase font-lg text-light font-bold bg-blue-logo hover:text-orange-logo py-2 px-6 rounded-lg">Entrar</Link> : <button onClick={onLogout} className="uppercase font-lg text-light font-bold bg-blue-logo hover:text-orange-logo py-2 px-6 rounded-lg cursor-pointer">LOGOUT</button>}
+      {!user ? <Link to='/login' className="uppercase font-lg text-light font-bold bg-blue-logo hover:text-orange-logo py-2 px-6 rounded-lg">Entrar</Link> : <button onClick={onLogout} className="uppercase font-lg text-light font-bold bg-blue-logo hover:text-orange-logo py-2 px-6 rounded-lg cursor-pointer">LOGOUT</button>}
     </nav>
   )
 }
