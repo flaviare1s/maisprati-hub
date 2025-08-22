5923e0ce5d4722f94183dbc8238e63575475b580
flaviare1s <flaviarster@gmail.com>
2025-08-22 11:43:19 -0300

    style: adjust padding

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,5 @@
 export const SectionTitle = ({ title }) => {
   return (
-    <h2 className="uppercase font-lg text-light bg-blue-logo py-2 px-6 rounded-full w-full sm:max-w-[500px] m-5 text-center font-bold">{title}</h2>
+    <h2 className="uppercase font-lg text-light bg-blue-logo py-2 px-6 rounded-full w-[90%] sm:max-w-[500px] m-5 text-center font-bold">{title}</h2>
   )
 }
