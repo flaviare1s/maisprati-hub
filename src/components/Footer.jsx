3da49c3e05709c75255185cb75b6b2a883bf052b
flaviare1s <flaviarster@gmail.com>
2025-09-01 09:57:48 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="w-full text-center p-5 text-blue-logo shadow border-t border-[#eee]">
-      <p>© 2025 - Todos os direitos reservados</p>
+    <footer className="w-full text-center p-5 bg-blue-logo shadow border-t border-[#eee]">
+      <p className="text-light font-semibold">© 2025 - Desenvolvido para a +PraTi</p>
     </footer>
   )
 }
