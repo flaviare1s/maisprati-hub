80e5aa48f325516796c0ac4f531692f4e95db674
flaviare1s <flaviarster@gmail.com>
2025-11-08 13:53:26 -0300

    style: fix loading style on submit button

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,5 +1,3 @@
-import { CustomLoader } from "./CustomLoader";
-
 export const SubmitButton = ({ label, isLoading = false, disabled = false, ...props }) => {
   return (
     <button
@@ -11,7 +9,7 @@ export const SubmitButton = ({ label, isLoading = false, disabled = false, ...pr
         }`}
         {...props}
     >
-      {isLoading ? <CustomLoader /> : label}
+      {isLoading ? "Carregando..." : label}
     </button>
   );
 };
